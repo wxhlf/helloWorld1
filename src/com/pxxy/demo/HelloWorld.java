e444229754163dfd4fc6a2e225608c236ba30316
wxhlf <704025299@qq.com>
2020-10-02 15:05:19 +0800

    second

--- src/com/pxxy/demo/HelloWorld.java
+++ src/com/pxxy/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.pxxy.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java again!");
     }
 }
